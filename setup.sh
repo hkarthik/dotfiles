3ccea1f9f76d8566346c117935e81a65927a8093
Karthik Hariharan <kar.hariharan@gmail.com>
2011-07-26 10:28:05 -0500

    adding Tmux and removing janus and rvm dependencies

--- setup.sh
+++ setup.sh
@@ -1,16 +1,8 @@
-if [ ! -d ~/.rvm/contrib ]; then
-  echo "dotfiles depend on rvm/contrib; installing/updating rvm"
-  bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
-fi
-echo "dotfiles depend on janus; cloning janus"
-for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
-git clone git://github.com/carlhuda/janus.git ~/.vim
-cd .vim
-rake
 echo "symlinking rc files"
-ln -nfs ~/bin/dotfiles/bash/bashrc ~/.bashrc
-ln -nfs ~/bin/dotfiles/bash/bash_profile ~/.bash_profile
-ln -nfs ~/bin/dotfiles/vim/vimrc.local ~/.vimrc.local
-ln -nfs ~/bin/dotfiles/vim/gvimrc.local ~/.gvimrc.local
-ln -nfs ~/bin/dotfiles/vim/janus.rake ~/.janus.rake
+ln -nfs ~/Projects/dotfiles/bash/bashrc ~/.bashrc
+ln -nfs ~/Projects/dotfiles/bash/bash_profile ~/.bash_profile
+ln -nfs ~/Projects/dotfiles/vim/vimrc.local ~/.vimrc.local
+ln -nfs ~/Projects/dotfiles/vim/gvimrc.local ~/.gvimrc.local
+ln -nfs ~/Projects/dotfiles/vim/janus.rake ~/.janus.rake
+ln -nfs ~/Projects/dotfiles/tmux/tmux.conf ~/.tmux.conf
 echo "finished setting up; have fun!"
