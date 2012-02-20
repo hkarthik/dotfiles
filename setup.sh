38541b81f166b2dc5388e5be14ade8a2cd8e6017
Karthik Hariharan <kar.hariharan@gmail.com>
2012-02-20 11:22:44 -0600

    adding vim scripts and tmuxinator scripts

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ ln -nfs ~/Projects/dotfiles/vim/vimrc.after ~/.vimrc.after
 ln -nfs ~/Projects/dotfiles/vim/vimrc.before ~/.vimrc.before
 ln -nfs ~/Projects/dotfiles/vim/janus.rake ~/.janus.rake
 ln -nfs ~/Projects/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -nfs ~/Projects/dotfiles/tmuxinator ~/.tmuxinator
 ln -nfs ~/Projects/dotfiles/.irbrc ~/.irbrc
 echo "finished setting up; have fun!"
