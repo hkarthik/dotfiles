43a68d1dcfbb020f5e03428f011176972fcf9a1f
Karthik Hariharan <kar.hariharan@gmail.com>
2012-07-11 21:23:23 -0400

    Fixing tmux

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ ln -nfs ~/Projects/dotfiles/zsh/zshrc ~/.zshrc
 ln -nfs ~/Projects/dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/Projects/dotfiles/vim/vim ~/.vim
 ln -nfs ~/Projects/dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -nfs ~/Projects/dotfiles/tmuxinator/* ~/.tmuxinator
 ln -nfs ~/Projects/dotfiles/.irbrc ~/.irbrc
 ln -nfs ~/Projects/dotfiles/ack/ackrc ~/.ackrc
 echo "finished setting up; have fun!"
