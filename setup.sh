4b53554fba73e21458736aab68383b86c6b72b1e
Karthik Hariharan <kar.hariharan@gmail.com>
2013-06-01 12:38:18 -0500

    Updating env files.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 echo "symlinking rc files"
 ln -nfs ~/Projects/dotfiles/zsh/zshrc ~/.zshrc
+ln -nfs ~/Projects/dotfiles/zsh/zshenv ~/.zshenv
 ln -nfs ~/Projects/dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/Projects/dotfiles/vim/vim ~/.vim
 ln -nfs ~/Projects/dotfiles/tmux/tmux.conf ~/.tmux.conf
