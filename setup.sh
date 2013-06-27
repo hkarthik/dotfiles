136b87e1bae4987feb809a5231bc74447709cf03
Karthik Hariharan <kar.hariharan@gmail.com>
2013-06-27 00:28:26 -0500

    Fixing symlink on functions for zsh.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 echo "symlinking rc files"
 ln -nfs ~/Projects/dotfiles/zsh/zshrc ~/.zshrc
 ln -nfs ~/Projects/dotfiles/zsh/zshenv ~/.zshenv
+ln -nfs ~/Projects/dotfiles/zsh/ ~/.zsh
 ln -nfs ~/Projects/dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/Projects/dotfiles/vim/vim ~/.vim
 ln -nfs ~/Projects/dotfiles/tmux/tmux.conf ~/.tmux.conf
